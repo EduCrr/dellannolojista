eaff699c0a4e6d211e1b377c97788dd8a9cfc418
Eduardo <agenciacriareof@gmail.com>
2022-10-30 19:38:09 -0300

    dellanno

---
+++ resources/views/home.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dellanno</title>
+    <link rel="stylesheet" href="/assets/css/style.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&amp;family=Montserrat:wght@600&amp;display=swap" as="font"/>
+
+</head>
+<body>
+
+
+    <div class="logo">
+        <img src="/assets/img/logo.png"/>
+    </div>
+    <header>
+        <div class="contentHeader">
+            <ul>
+                <li>
+                    <a href="#">Quero ser lojista</a>
+                </li>
+                    <li>
+                    <a href="#">A marca</a>
+                </li>
+                    <li>
+                    <a href="#">Por que ser lojista Dell Anno</a>
+                </li>
+                    <li>
+                    <a href="#">Unicasa</a>
+                </li>
+                <li>
+                    <a href="#">Passo a passo</a>
+                </li>
+                <li>
+                    <a href="#">Depoimentos</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <section class="interest">
+        <img class="interestImg" src="/assets/img/teste.jpg"/>
+        <div class="interestContent">
+           <div class="interestContainer">
+                <div class="interestContentText">
+                    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</h1>
+                </div>
+                <div class="interestContentForm">
+                    <h2>Preencha o formulário</h2>
+                    <p>E vamos conversar melhor </p>
+                    <form>
+                        <div class="all">
+                            <input placeholder="Nome"/>
+                        </div>
+                        <div class="all">
+                            <input placeholder="e-mail"/>
+                        </div>
+                        <div class="divide">
+                           <select name="" id="">
+                            <option>teste</option>
+                           </select>
+                            <input placeholder="telefone / whatsapp"/>
+                        </div>
+                        <div class="check">
+                            <input type="checkbox"/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed  elit, se  elit, se  </p>
+                        </div>
+                        <div class="all">
+                            <button>Enviar</button>
+                        </div>
+                    </form>
+                </div>
+           </div>
+        </div>
+    </section>
+    <section class="high">
+        <div class="highContent">
+            <h1>High End</h1>
+            <h1>Em qualquer</h1>
+            <h1>Ponto de vista</h1>
+        </div>
+    </section>
+    <section class="store">
+        <div class="storeContent">
+            <div class="storeContentLeft">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div class="logoStore">
+                    <img src="/assets/img/logo.png"/>
+                    <span class="lineV"></span>
+                </div>
+            </div>
+            <div class="storeContentRight">
+                <div class="slide">
+                    <div class="slideSingle">
+                        <img src="/assets/img/teste2.jpg"/>
+                    </div>
+                    <div class="slideSingle">
+                        <img src="/assets/img/teste2.jpg"/>
+                    </div>
+                    <div class="slideSingle">
+                        <img src="/assets/img/teste2.jpg"/>
+                    </div>
+                </div>
+            </div>
+            <div class="backgroundSlide"></div>
+        </div>
+    </section>
+    <section class="relation">
+        <div class="backgroundRelation"></div>
+        <div class="relationContent">
+            <div class="relationLeft">
+                <img src="/assets/img/teste.jpg"/>
+            </div>
+            <div class="relationRight">
+                
+                <h1>Relação sublime com o trabalho, apego pelo extraordinário<br/> e apetite estético insaciável</h1>
+            </div>
+           
+        </div>
+    </section>
+    <section class="moreText">
+        <div class="moreTextContent">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </section>
+    <section class="infos">
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Growth Suport</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+        <div class="singleInfo">
+            <div class="singleInfoTop">
+                <img src="/assets/img/icon.png"/>
+                <h3>Suporte de Arquitetura</h3>
+            </div>
+            <div class="singleInfoBottom">
+                <p>Acompanhamento de todas etapas até a abertura da loja</p>
+            </div>
+        </div>
+    </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="/assets/js/functions.js"></script>
+
+</body>
+</html>
+<!-- 
+//https://codepen.io/sinzo/pen/ZEzdjZK -->
